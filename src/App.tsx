46cf2d0aefc399696058250981798d3ecf74695c
CocoOreo <songhaoranuestc@163.com>
2021-03-26 16:53:42 +0800

    feat(button):finish button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Button from './components/Button/button'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // 展示Button组件
+    <div className="container">
+      <Button btnType='primary' onClick={() => {alert('This is a primary button')}}>Large Primary</Button>
+      <Button btnType='danger'>Danger</Button>
+      <Button btnType='link' href='https://www.baidu.com'>Baidu</Button>
+      <Button >Default</Button>
     </div>
   );
 }
